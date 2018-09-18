34c2915d233e3eeacd74059cd9aef4b51a950817
Kumar Shantnu <kumarshantnu@Kumars-MacBook-Air.local>
2018-09-18 16:20:39 +0530

    express and angulat deployment

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const {UserSubmissions} = require('./models/usersubmissions');
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.static(__dirname + '/public/dist'));
 
 app.use(bodyparser.json());
+app.get('/*', function (req, res) {
+  res.sendfile(path.join(__dirname + '/public/dist/index.html'));
+})
 
-
-
-app.post('/getsuggestion',(req,res) => {
+app.post('/getsuggestion', (req, res) => {
   const tagsToSearchFor = req.body.tags;
   const queries = [];
   tagsToSearchFor.map(item => {
@@ -21,28 +23,25 @@ app.post('/getsuggestion',(req,res) => {
     queries.push(UserSubmissions.find({medicines: item}));
     queries.push(UserSubmissions.find({doctors: item}));
   });
-  Promise.all(queries).then(resultArray => {
-    res.send(resultArray);
-  })
+  Promise
+    .all(queries)
+    .then(resultArray => {
+      res.send(resultArray);
+    })
 })
 
-
-
-app.post('/submitsuggestion',(req,res) => {
-  const userSubmissions = new UserSubmissions({
-      symptoms: req.body.symptoms,
-      doctors: req.body.doctors,
-      medicines: req.body.medicines,
-      comments: req.body.comments,
-  });
-  userSubmissions.save().then(userSubmittedDoc => {
-    res.send(userSubmittedDoc);
-  }).catch((err) => {
-    console.log(err);
-  })
+app.post('/submitsuggestion', (req, res) => {
+  const userSubmissions = new UserSubmissions({symptoms: req.body.symptoms, doctors: req.body.doctors, medicines: req.body.medicines, comments: req.body.comments});
+  userSubmissions
+    .save()
+    .then(userSubmittedDoc => {
+      res.send(userSubmittedDoc);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 })
 
-app.listen(port,() => {
-    console.log(`app started at ${port}`);
+app.listen(port, () => {
+  console.log(`app started at ${port}`);
 })
-
