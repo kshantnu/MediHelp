bc6791bf224c0bb891abb66ca64f855ab298b594
Kumar Shantnu <kumarshantnu@Kumars-MacBook-Air.local>
2018-09-18 15:45:07 +0530

    git setup for heroku

--- db-config.js
+++ db-config.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/MediHelp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MediHelp');
 module.exports = {mongoose};
